eb61ec9367ede74e64afad0bdeba713e4f40dc8e
sergii <sergiishevchenko017@gmail.com>
2018-11-05 22:26:48 +0300

    fourth commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 window.addEventListener('DOMContentLoaded', function(){
     'use strict';
     let calc = require('./js/parts/calc.js'),
-        forms = require('./js/parts/forms.js'),
         memory = require('./js/parts/memory.js'),
         modal = require('./js/parts/modal.js'),
         pict = require('./js/parts/pict.js'),
@@ -9,7 +8,6 @@ window.addEventListener('DOMContentLoaded', function(){
         timer = require('./js/parts/timer.js');
 
     calc();
-    forms();
     memory();
     modal();
     pict();
--- src/js/parts/calc.js
+++ src/js/parts/calc.js
@@ -49,10 +49,12 @@ function calc() {
         if (target && target.classList.contains('icons')) {
             for (let i = 0; i < balconIcons.length; i++) {
                 if (target == balconIcons[i]) {
+                    balconIcons[i].classList.add('iconsA');
                     hideIcons(0);
                     showIcons(i);
                     break;
                 }
+                balconIcons[i].classList.remove('iconsA');
             }
         }
 
@@ -115,6 +117,97 @@ function calc() {
         popupCalc.classList.remove('more-splash');
         document.body.style.overflow = '';
     });
+
+    // Forms
+
+    let message = {
+        loading: 'Отправка',
+        success: 'Отправлено!',
+        failure: 'Ошибка...'
+        };
+
+    let form = document.getElementsByTagName('form'),
+        typeWindows = document.querySelectorAll('.big_icons'),
+        typeCovering = document.querySelector('.popup_calc_profile_content select'),
+        checkbox = document.querySelectorAll('.checkbox-custom'),
+        statusMessage = document.createElement('div');
+
+    let formData = new FormData();
+    
+    let inputTel = document.getElementsByName('user_phone');
+    
+    for (let i = 0; i < inputTel.length; i++) {
+        inputTel[i].addEventListener('input', function () {
+            this.value = this.value.replace(/\D/, '');
+        });
+    }
+    
+    checkbox[0].addEventListener('click', function() {
+        checkbox[1].checked === false;
+        formData.append('Стиль остекленения: ', "Cold");
+    });
+
+    checkbox[1].addEventListener('click', function() {
+        checkbox[0].checked === false;
+        formData.append('Стиль остекленения: ', "Warm");
+    });
+
+    statusMessage.classList.add('status');
+
+    popupCalcButton.addEventListener('click', function() {
+        formData.append(widthWindow.getAttribute('placeholder'), widthWindow.value);
+        formData.append(heightWindow.getAttribute('placeholder'), heightWindow.value);
+        for (let i = 0; i < typeWindows.length; i++) {
+            formData.append('Type of window: ', typeWindows[i].id)
+        }
+    });
+
+    popupCalcProfileButton.addEventListener('click', function() {
+        formData.append(typeCovering.getAttribute('id'), typeCovering.value);
+    });
+
+    for (let i = 0; i < form.length; i++) {
+
+        form[i].addEventListener('submit', function(event) {
+            event.preventDefault();
+            form[i].appendChild(statusMessage);
+    
+            let request = new XMLHttpRequest();
+                request.open('POST', 'server.php');
+                request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+    
+            let input = form[i].querySelectorAll('.form_input');
+
+            for (let i = 0; i < input.length; i++) {
+                formData.append(input[i].getAttribute('name'), input[i].value);
+            }
+        
+            let obj = {};
+
+            formData.forEach(function(value, key) {
+                obj[key] = value;
+            });
+    
+            let json = JSON.stringify(obj);
+    
+            request.send(json);
+    
+            request.addEventListener('readystatechange', function() {
+                if (request.readyState < 4 ) {
+                    statusMessage.innerHTML = message.loading;
+                } else if (request.readyState === 4 && request.status == 200) {
+                    statusMessage.innerHTML = message.success;
+                } else {
+                    statusMessage.innerHTML = message.failure;
+                }
+            });
+    
+            for (let i = 0; i < input.length; i++){
+                input[i].value = '';
+            }
+    
+        });
+    }
 }
 
 module.exports = calc;
